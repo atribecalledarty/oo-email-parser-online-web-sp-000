f2c897917323eae7af1b8026b325010959e8755b
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 04:30:35 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,5 +3,11 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser
+  attr_accessor :list_of_emails
   
+  def initialize(string)
+    @list_of_emails = string.split(" ")
+    @list_of_emails = @list_of_emails.each {|email| email.chomp(",")}
+    
+  end
 end
